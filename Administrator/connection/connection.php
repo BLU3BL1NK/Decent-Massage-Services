da4660de4660135007053aca97fd4bb4e36232c3
BLU3BL1NK <112202552+BLU3BL1NK@users.noreply.github.com>
2025-12-04 00:38:07 +0800

    Update connection.php

--- Administrator/connection/connection.php
+++ Administrator/connection/connection.php
@@ -1,1 +1,10 @@
-
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$database = "decent";
+	$cnn = new mysqli($servername, $username, $password, $database);
+	if ($cnn->connect_error) {
+		die("Connection failed: " . $cnn->connect_error);
+	}
+?>
